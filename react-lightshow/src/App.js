d78fe2b2af51ebff9cf8a04bc1b9945fb1386024
Rudson Varona <rjvarona16@gmail.com>
2020-05-28 22:29:35 -0700

    adding canvas and particles

--- react-lightshow/src/App.js
+++ react-lightshow/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,3 +10,30 @@ function App() {
 }
 
 export default App;
+
+
+
+function Canvas() {
+
+  const [canvasWidth, setcanvasWith] = useState(window.innerWidth);
+  
+  const [canvasHeight, setcanvasHeight] = useState(window.innerHeight);
+
+  const [particleArray, setParticleArray] = useState(Array(2).fill(null));
+
+  const [mouse, setMouse] = useState({
+    x: null,
+    y: null,
+    radius: ((canvasHeight/80) * (canvasWidth/80))
+  });
+
+  window.addEventListener('mousemove',
+    function(event){
+      let copyMouse = {
+        x : event.x,
+        y : event.y
+      }
+    }  
+  );
+
+}
